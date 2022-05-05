7fcc78773b12d110bc906dd7a3768fbcd4b38caa
Sridhar Ratnakumar <srid@srid.ca>
2022-05-04 21:04:23 -0400

    get rid of foreman, tailwind3

--- flake.nix
+++ flake.nix
@@ -31,7 +31,6 @@
             fourmolu
             pkgs.nixpkgs-fmt
             tailwind-haskell
-            pkgs.foreman
             pkgs.treefmt
           ];
           project = returnShellEnv:
--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,47 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
+{-# HLINT ignore "Use camelCase" #-}
+
 module Main where
 
-import Ema (runSite)
-import Ema.Multi (MultiRoute)
-import Generics.SOP
-import Site.Amb (Route)
-import Site.StaticSite (StaticPath)
+import Ema
+import Generics.SOP qualified as SOP
+import Text.Blaze.Html.Renderer.Utf8 qualified as RU
+import Text.Blaze.Html5 ((!))
+import Text.Blaze.Html5 qualified as H
+import Text.Blaze.Html5.Attributes qualified as A
+
+data Route = Route_Index
+  deriving stock
+    (Show, Eq, Ord, Generic)
+  deriving anyclass
+    (SOP.Generic, SOP.HasDatatypeInfo, HasModel, IsRoute)
+
+instance CanRender Route where
+  routeAsset enc m Route_Index =
+    Ema.AssetGenerated Ema.Html . RU.renderHtml $ do
+      H.docType
+      H.html ! A.lang "en" $ do
+        H.head $ do
+          H.meta ! A.charset "UTF-8"
+          -- This makes the site mobile friendly by default.
+          H.meta ! A.name "viewport" ! A.content "width=device-width, initial-scale=1"
+          H.link
+            ! A.href "https://unpkg.com/tailwindcss@2/dist/tailwind.min.css"
+            ! A.rel "stylesheet"
+            ! A.type_ "text/css"
+          H.title "Amb"
+          H.base ! A.href "/"
+        H.body $ do
+          H.div ! A.class_ "container mx-auto mt-8 p-2" $ do
+            H.h1 ! A.class_ "text-3xl font-bold" $ "Amb"
+            "todo"
+    where
+      _routeElem r' = do
+        H.a ! A.class_ "text-red-500 hover:underline" ! _routeHref r'
+      _routeHref r' =
+        A.href (fromString . toString $ Ema.routeUrl enc m r')
 
 main :: IO ()
 main =
-  void $ runSite @(MultiRoute '[Route, StaticPath]) (I () :* I () :* Nil)
+  void $ runSite @Route ()
