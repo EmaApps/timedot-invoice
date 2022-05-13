d9d0005417f76c792d02c20953a4f2f667ba006c
Sridhar Ratnakumar <srid@srid.ca>
2022-05-12 20:56:37 -0400

    cli: add --host/--port

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -16,8 +17,7 @@ import Heist qualified as H
 import Heist.Interpreted qualified as HI
 import Main.Utf8 (withUtf8)
 import Options.Applicative
-import System.Directory (makeAbsolute)
-import System.FilePath (takeDirectory, takeFileName, (</>))
+import System.FilePath (splitFileName, (</>))
 import System.FilePattern (FilePattern)
 import System.UnionMount qualified as UM
 import TI.HLedger qualified as HLedger
@@ -49,8 +49,8 @@ data FileType
   | FileType_Yaml
   deriving stock (Eq, Show, Ord)
 
-inputPatterns :: CLI -> [(FileType, FilePattern)]
-inputPatterns CLI {cliTimedotFile} =
+filePatterns :: CLI -> [(FileType, FilePattern)]
+filePatterns CLI {cliTimedotFile} =
   [ (FileType_Timedot, cliTimedotFile)
   , (FileType_Tpl, cliTimedotFile <> ".tpl")
   , (FileType_Yaml, cliTimedotFile <> ".tpl.yaml")
@@ -61,7 +61,7 @@ instance EmaSite Route where
   siteInput _ _ input@CLI {..} = do
     model0 <- liftIO emptyModel
     fmap Dynamic $
-      UM.mount cliBaseDir (inputPatterns input) [".*"] model0 $
+      UM.mount cliBaseDir (filePatterns input) [".*"] model0 $
         \tag fp act -> case tag of
           FileType_Timedot -> case act of
             UM.Refresh _ () -> do
@@ -82,57 +82,51 @@ instance EmaSite Route where
             liftIO $ putStrLn $ "TODO: YAML handling " <> fp
             pure id
   siteOutput _ m Route_Index =
-    Ema.AssetGenerated Ema.Html . renderTpl m $ do
-      "invoice:errors" ## H.listSplice (modelErrors m) "error" $ \err -> do
-        "error:err" ## HI.textSplice err
+    Ema.AssetGenerated Ema.Html . renderTpl (modelTemplateState m) $ do
+      "invoice:errors" ## H.listSplice (modelErrors m) "error" $ \err -> "error:err" ## HI.textSplice err
       "invoice:hours" ## H.listSplice (modelHours m) "hour" $ \(day, clients) -> do
         "hour:day" ## HI.textSplice (show day)
         "hour:clients" ## H.listSplice (Map.toList clients) "client" $ \(client, hours) -> do
           "client:name" ## HI.textSplice (show client)
           "client:hours" ## HI.textSplice (show hours)
     where
-      renderTpl :: Model -> H.Splices (HI.Splice Identity) -> LByteString
-      renderTpl (Model tmplSt _ _) args =
+      renderTpl :: H.TemplateState -> H.Splices (HI.Splice Identity) -> LByteString
+      renderTpl tmplSt args =
         -- TODO: don't hardcode template name
         either errorHtml id $ H.renderHeistTemplate "hours.timedot" args tmplSt
         where
           errorHtml err = "<span style=\"text-color: red\">" <> encodeUtf8 err <> "</span>"
 
 main :: IO ()
-main = do
-  withUtf8 $ do
-    cli <- parseCli
-    putStrLn $ "Running with args: " <> show cli
-    void $ runSiteWithCli @Route emaCli cli
-  where
-    -- TODO: Allow setting port
-    emaCli = Ema.CLI.Cli (Some $ Ema.CLI.Run ("127.0.0.1", 9092)) False
+main = withUtf8 $ do
+  cli <- parseCli
+  putStrLn $ "Running with args: " <> show cli
+  void $ runSiteWithCli @Route (cliEma cli) cli
 
 data CLI = CLI
   { -- | The base directory containing the timedot file. Ema will scan this.
     cliBaseDir :: FilePath
   , -- | Filename of the timedot file under the base directory
     cliTimedotFile :: FilePath
+  , -- | The Ema CLI settings to start the live server from.
+    cliEma :: Ema.CLI.Cli
   }
   deriving stock (Eq, Show)
 
 parseCli :: IO CLI
 parseCli =
-  mkCli =<< execParser cliParserInfo
+  execParser cliParserInfo
   where
-    mkCli :: FilePath -> IO CLI
-    mkCli fp = do
-      cliBaseDir <- makeAbsolute $ takeDirectory fp
-      let cliTimedotFile = takeFileName fp
-      pure $ CLI {..}
-
     -- TODO: Add more CLI params
     -- - Duration (default: 2 weeks)
-    cliParser :: Parser FilePath
+    cliParser :: Parser CLI
     cliParser = do
-      argument str (metavar "TIMEDOT_FILE" <> value "./hours.timedot")
+      let fileP = argument str (metavar "TIMEDOT_FILE" <> value "./hours.timedot")
+      cliEma <- flip Ema.CLI.Cli False . Some . Ema.CLI.Run <$> Ema.CLI.hostPortParser
+      (cliBaseDir, cliTimedotFile) <- (fst &&& snd) . splitFileName <$> fileP
+      pure $ CLI {..}
 
-    cliParserInfo :: ParserInfo FilePath
+    cliParserInfo :: ParserInfo CLI
     cliParserInfo =
       info
         (versionOption <*> cliParser <**> helper)
