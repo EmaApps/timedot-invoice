87f5727ebe799f90b26aa80bdc8524bb8bc73137
Sridhar Ratnakumar <srid@srid.ca>
2022-05-14 10:47:30 -0400

    document Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE UndecidableInstances #-}
 
@@ -23,6 +22,11 @@ import System.UnionMount qualified as UM
 import TI.HLedger qualified as HLedger
 import TI.Heist qualified as H
 
+{- | The "index.html" route.
+
+ Route types are the lynchpin of Ema apps, thus we need this sophisticated type
+ even if our site will generate a single HTML file.
+-}
 data Route = Route_Index
   deriving stock
     (Show, Eq, Ord, Generic)
@@ -32,36 +36,55 @@ data Route = Route_Index
     (IsRoute)
     via (SingleModelRoute Model Route)
 
+-- | All the data required to generate our "index.html" file
 data Model = Model
   { modelTemplateState :: H.TemplateState
   , modelHours :: HLedger.TimedotEntries
   , modelErrors :: [Text]
   }
 
+-- | The empty model corresponding to the "index.html" that has no data (yet).
 emptyModel :: IO Model
 emptyModel = do
   ts <- H.emptyTemplateState
   pure $ Model ts mempty mempty
 
+-- | The type of files we want to watch (for changes) on disk.
 data FileType
   = FileType_Timedot
   | FileType_Tpl
   | FileType_Yaml
   deriving stock (Eq, Show, Ord)
 
-filePatterns :: CLI -> [(FileType, FilePattern)]
-filePatterns CLI {cliTimedotFile} =
-  [ (FileType_Timedot, cliTimedotFile)
-  , (FileType_Tpl, cliTimedotFile <> ".tpl")
-  , (FileType_Yaml, cliTimedotFile <> ".tpl.yaml")
+{- | Given a "foo.timedot" what kind of files do we want to observe
+for changes on?
+-}
+filePatternsToWatch :: FilePath -> [(FileType, FilePattern)]
+filePatternsToWatch timedotFile =
+  [ (FileType_Timedot, timedotFile)
+  , (FileType_Tpl, timedotFile <> ".tpl")
+  , (FileType_Yaml, timedotFile <> ".tpl.yaml")
   ]
 
+{- Define the Ema site
+
+  - `siteInput` defines the "input" data required to generate the site. This data
+  is a Ema `Dynamic`, as in - it is time-varying.
+  - `siteOutput` defines the "output" data, which in our case is a HTML string
+  to be written to "index.html" route.
+  - `SiteArg` (which is `CLI`) is what the `siteInput` is parametrized over. Its
+  value is passed over to the `runSite` function which is called from `main`.
+
+-}
 instance EmaSite Route where
   type SiteArg Route = CLI
-  siteInput _ _ input@CLI {..} = do
+  siteInput _ _ CLI {..} = do
     model0 <- liftIO emptyModel
+    -- We use the unionmount library to map the filesystem to in-memory `Model`,
+    -- and update it over file as the files change. This abstraction is wrapped
+    -- in a `Dynamic`.
     fmap Dynamic $
-      UM.mount cliBaseDir (filePatterns input) [".*"] model0 $
+      UM.mount cliBaseDir (filePatternsToWatch cliTimedotFile) [".*"] model0 $
         \tag fp act -> case tag of
           FileType_Timedot -> case act of
             UM.Refresh _ () -> do
@@ -82,6 +105,8 @@ instance EmaSite Route where
             liftIO $ putStrLn $ "TODO: YAML handling " <> fp
             pure id
   siteOutput _ m Route_Index =
+    -- We use Heist templates for HTML. Here we define the variables required to
+    -- render that template. cf. https://srid.ca/heist-start
     Ema.AssetGenerated Ema.Html . renderTpl (modelTemplateState m) $ do
       "invoice:errors" ## H.listSplice (modelErrors m) "error" $ \err -> "error:err" ## HI.textSplice err
       "invoice:hours" ## H.listSplice (modelHours m) "hour" $ \(day, clients) -> do
